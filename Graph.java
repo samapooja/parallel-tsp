e1dddd3caef184e1db6920c37dbf00149b894e4a
rclark72 <rclark72@bc249cf0-e87d-11de-a198-37193048d1ed>
2010-01-18 00:35:47 +0000

    Removed a lot of excessive code that we don't need

--- Graph.java
+++ Graph.java
@@ -15,188 +15,52 @@ import java.text.DecimalFormat;
  * @author   Robert Clark
  */
 public class Graph {
-	private double weights[][];
-	private Vertex vertices[];
-	private Edge edges[];
+	private long weights[][];
 	private int graphSize;
-	private boolean randomGraph = true;
 	private int edgeCount;
-	private int vertexCount = 0;
-
-	Graph(int size, int seed) {
-		int nextX, nextY;
-		nextX=0;nextY=0;
-		graphSize = size;
-		Random xGen = new Random(seed);
-		Random yGen = new Random(2*seed);
-		vertices = new Vertex[size];
-		edges = new Edge[(((size*size)-size)/2)+size];
-		weights = new double[size][size];
-		
-		// Generate all of the vertices
-		while(vertexCount < size){
-			nextX = Math.abs(xGen.nextInt(size));
-			nextY = Math.abs(yGen.nextInt(size));
-			// If the X is taken then define new coords
-			boolean doCont = false;
-			for(int y = 0; y < vertexCount; y++){
-				if(vertices[y].getX() == nextX)
-					doCont = true;
-			}
-			if (doCont == true)
-				continue;
-			vertices[vertexCount] = new Vertex(nextX, nextY, vertexCount);
-			vertexCount++;
-			//x++;
-		}
-		// Connect all the vertices
-		edgeCount = 0;
-		for(int i = 0; i < size; i++) {
-			for (int j = i; j < size; j++) {
-				addEdge(i, j);
-			}
-		}
-		if(size <= 10) {
-			printVertices();
-			printMatrix();
-		}
-	}
 
 	Graph(int size) {
 		graphSize = size;
-		randomGraph = false;
-		weights = new double[size][size];
-		vertices = new Vertex[size];
-		edges = new Edge[size];
+		weights = new long[size][size];
 		edgeCount = 0;
 	}
 
-	public void addEdge(int i, int j) {
-		edges[edgeCount] = new Edge(vertices[i], vertices[j]);
-		weights[i][j] = edges[edgeCount].getCost();
-		weights[j][i] = edges[edgeCount].getCost();
-		edgeCount++;
-	}
-	
-	public void addVertex(Vertex newVert) {
-		int index = newVert.getIndex();
-		//int x = newVert.getX();
-		//int y = newVert.getY();
-		vertices[index] = newVert;//new Vertex(x, y, index);
-		vertexCount++;
-	}
-
-	public void addVertex(int x, int y, int index) {
-		vertices[index] = new Vertex(x, y, index);
-		vertexCount++;
-	}
-
-	public Vertex getVertex(int x) {
-		return vertices[x];
-	}
-	public void sortVertices() {
-		quickSortV(0, vertices.length-1);
-	}
-	private void quickSortV(int p, int r) {
-		if(p < r) {
-			int q = partitionVertices(p,r);
-			quickSortV(p,q-1);
-			quickSortV(q+1,r);
-		}
-	}
-	private int partitionVertices( int p, int r ) {
-		Vertex x = vertices[r];
-		int i = p-1;
-		for(int j = p; j <= (r-1); j++) {
-			if(vertices[j].compareTo(x) <= 0) {
-				i += 1;
-				swapVertices(i,j);
+	public void randomize(int max) {
+		Random rng = new Random();
+		for(int x = 0; x < graphSize; x++) {
+			for(int y = x; y < graphSize; y++) {
+				if(x == y) {
+					weights[x][y] = 0;
+				} else {
+					long next = rng.nextInt(max);
+					weights[x][y] = next;
+					weights[y][x] = next;
+				}
 			}
 		}
-		swapVertices(i+1,r);
-		return i+1;
-	}
-	private void swapVertices( int i, int j ) {
-		Vertex temp = vertices[i];
-		vertices[i] = vertices[j];
-		vertices[j] = temp;
-	}
-	public Edge[] getEdges() {
-		return edges;
-	}
-	
-	public void sortEdges() {
-		quickSort(0, edges.length-1);
 	}
 
-	private void quickSort(int p, int r) {
-		if(p < r) {
-			int q = partition(p, r);
-			quickSort(p, q-1);
-			quickSort(q+1, r);
-		}
-	}
-
-	private int partition(int p, int r) {
-		Edge x = edges[r];
-		int i = p-1;
-		for(int j = p; j <= (r-1); j++) {
-			if(edges[j].compareTo(x) <= 0){
-				i += 1;
-				swapEdges(i, j);
-			}
-		}
-		swapEdges(i+1, r);
-		return i+1;
-	}
-
-	private void swapEdges( int i, int j ) {
-		Edge temp = edges[i];
-		edges[i] = edges[j];
-		edges[j] = temp;
-	}
-
-	public void printVertices () {
-		System.out.println("X-Y Coordinates:");
-		for(int x = 0; x < graphSize; x++){
-			System.out.print("v" + vertices[x].getIndex() + ": (" + vertices[x].getX()); 
-			System.out.print("," + vertices[x].getY() + ") ");
-		}
-		System.out.println("\n");
-	}
-	
-	public double[][] getMatrix() {
+	public long[][] getMatrix() {
 		return weights;
 	}
 
 	public void printMatrix () {
 		System.out.println("Adjacency matrix of graph weights:\n");
-		System.out.print("   ");
-		DecimalFormat formatter = new DecimalFormat("0.00");
+		System.out.print("\t");
 		for(int x = 0; x < graphSize; x++) 
-			System.out.print("  " + x + "   ");
+			System.out.print(x + "\t");
+
 		System.out.println("\n");
 		for(int x = 0; x < graphSize; x++){
-			System.out.print(x + "  ");
+			System.out.print(x + "\t");
 			for(int y = 0; y < graphSize; y++) {
 				if(weights[x][y] == 0) {
-					System.out.print("0.00  ");
+					System.out.print("0\t");
 				}else{
-					System.out.print(formatter.format(weights[x][y]) + "  ");
+					System.out.print(weights[x][y] + "\t");
 				}
 			}
 			System.out.println("\n");
 		}
-	}
-	
-	public void printEdges() {
-		System.out.println("Edge listing");
-		for(int x = 0; x < edges.length; x++){
-			System.out.println(edges[x]);
-		}
-	}
-
-	public int vertCount() {
-		return vertexCount;
 	}
 }
