603b2fd5b2e2422f8e25b70f8abc71ca0573eed6
rclark72 <rclark72@bc249cf0-e87d-11de-a198-37193048d1ed>
2010-02-12 05:31:58 +0000

    Beginning of refactor.

--- OptimalTSP.java
+++ OptimalTSP.java
@@ -6,17 +6,22 @@ import java.util.Stack;
  * @author   Robert Clark
  */
 public class OptimalTSP {
-	long[][] graphMatrix;
+	long[][] weightMatrix;
 	int[] optimalPath;
 	long optimalCost;
 	int max_depth = 8;
-	long[] matrixMins;
-	long[] matrixMins2;
+
+	public class TSPState {
+		long[][] matrix;
+		public TSPState(long[][] weightMatrix) {
+			this.matrix = weightMatrix;
+		}
+		public long[][] matrix() { return matrix; }
+	}
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		String inputName = args[0];
-		int depth = Integer.parseInt(args[1]);
 
 		Graph theGraph = new Graph();
 		try {
@@ -26,157 +31,93 @@ public class OptimalTSP {
 			System.exit(0);
 		}
 
-		OptimalTSP solver = new OptimalTSP(theGraph, depth);
+		OptimalTSP solver = new OptimalTSP(theGraph);
 
-		solver.branch();
-
-		System.out.println();
-		solver.displayOptimal();
 		long stop = System.currentTimeMillis();
 		System.out.println("Runtime for optimal TSP   : " + (stop-start) + " milliseconds");
 		System.out.println();
 	}
 
-	OptimalTSP(Graph inputGraph, int depth) {
-		graphMatrix = inputGraph.getMatrix();
-		optimalCost = Long.MAX_VALUE;
-		if(graphMatrix.length <= depth)
-			max_depth = graphMatrix.length - 1;
-		else
-			max_depth = depth;
-
-		genMatrixMins();
+	OptimalTSP(Graph inputGraph) {
+		weightMatrix = inputGraph.getMatrix();
+		inputGraph.printMatrix();
+		long lower_bound = OptimalTSP.reduce(inputGraph.getMatrix());
+		inputGraph.printMatrix();
+		System.out.println(lower_bound);
 	}
 
-	/**
-	 * Generates an array of all nodes
-	 **/
-	public int[] nodeList() {
-		int[] nodes = new int[graphMatrix.length-1];
-		for(int x = 1; x < graphMatrix.length; x++) {
-			nodes[x-1] = x;
-		}
-		return nodes;
+	public void run() {
+		Stack<TSPState> rightStack = new Stack<TSPState>();
+
 	}
 
-	/**
-	 * Print out the current optimal path
-	 **/
-	public void displayOptimal() {
-		System.out.print("Optimal distance: " + optimalCost);
-		System.out.print(" for path ");
-		for(int x = 0; x < optimalPath.length; x++) {
-			System.out.print(optimalPath[x] + " ");
-		}
-		System.out.println("0");
-	}
-
-	public long testPath(int[] testPath) {
-		long cost = 0;
-		int x = 0;
-
-		for(x = 0; x < (testPath.length-1); x++) {
-			cost += graphMatrix[testPath[x]][testPath[x+1]];
-			if(cost >= optimalCost) {
-				return 0;
+	public static int[] bestCoord(long[][] matrix) {
+		int[] retVal = new int[2];
+		for(int x = 0; x < matrix.length; x++) {
+			long min = matrix[x][0];
+			for(int y = x; y < matrix[x].length; y++) {
+				min = Math.min(min, matrix[x][y]);
 			}
 		}
-		cost += graphMatrix[testPath[x]][0];
-		
-		if(cost < optimalCost) {
-			optimalPath = testPath;
-			optimalCost = cost;
-		}
-		return cost;
+		return retVal;
 	}
 
-	/**
-	 * Recursively branches until it reaches the max_depth
-	 * it then binds and calculates the shortest path cost
-	 * at the specified branch.
-	 **/
-	public void branch() {
-		Stack<SearchState> branchStack = new Stack<SearchState>();
-		SearchState start_state = new SearchState(nodeList(), this.graphMatrix);
-		branchStack.push(start_state);
-
-		while(!branchStack.empty()) {
-			SearchState cur_state = branchStack.pop();
-			
-			// depth reaches the maximum, stop branching
-			if(cur_state.depth > max_depth) {
-				int[] new_path = cur_state.pathAndPool();
-				bind(new_path, cur_state.depth);
-			} else {
-				for(int x = 0; x < cur_state.freeCount(); x++) {
-					SearchState new_state = cur_state.genNextState(x);
-					long heuristic = calcHeuristic(new_state);
-					if(heuristic < optimalCost) {
-						branchStack.push(new_state);
-					}
+	public static long reduce(long[][] matrix) {
+		long lower_bound = 0;
+		for(int x = 0; x < matrix.length; x++) {
+			long min = matrix[x][0];
+			for(int y = x; y < matrix[x].length; y++) {
+				min = Math.min(min, matrix[x][y]);
+			}
+			for(int y = x; y < matrix[x].length; y++) {
+				if(matrix[x][y] != Long.MAX_VALUE){
+					matrix[x][y] = matrix[x][y] - min;
+					matrix[y][x] = matrix[y][x] - min;
 				}
 			}
+			lower_bound += min;
 		}
-	}
-
-	public long calcHeuristic(SearchState state) {
-		// calculate heuristic
-		long heuristic_val = 0;
-		for(int x : state.getFreeNodes()) {
-			heuristic_val += matrixMins[x];
-			heuristic_val += matrixMins2[x];
-		}
-		heuristic_val = heuristic_val / 2;
-		heuristic_val += matrixMins[0];
-		heuristic_val += state.cost;
-		return heuristic_val;
-	}
-	
-	/**
-	 * This method generates the matrix minimum values for each row
-	 **/
-	public void genMatrixMins() {
-		matrixMins = new long[graphMatrix.length];
-		matrixMins2 = new long[graphMatrix.length];
-		for(int index = 0; index < graphMatrix.length; index++) {
-			long minVal = 0;
-			if(index == 0) {
-				minVal = graphMatrix[index][1];
-			} else {
-				minVal = graphMatrix[index][0];
+		for(int x = 0; x < matrix.length; x++) {
+			long min = matrix[0][x];
+			for(int y = x; y < matrix[x].length; y++) {
+				min = Math.min(min, matrix[y][x]);
 			}
-			long minVal2 = minVal;
-			for(int x = 0; x < graphMatrix[index].length; x++) {
-				if(x != index && graphMatrix[index][x] < minVal) {
-					minVal2 = minVal;
-					minVal = graphMatrix[index][x];
-				} else if(x != index && graphMatrix[index][x] < minVal2) {
-					minVal2 = graphMatrix[index][x];
+			for(int y = x; y < matrix[x].length; y++) {
+				if(matrix[x][y] != Long.MAX_VALUE){
+					matrix[x][y] = matrix[x][y] - min;
+					matrix[y][x] = matrix[y][x] - min;
 				}
 			}
-			matrixMins[index] = minVal;
-			matrixMins2[index] = minVal2;
+			lower_bound += min;
 		}
+		return lower_bound;
 	}
 
-
-	public void bind(int[] input, int offset) {
-		if(input.length - offset <= 1) {
-			int[] output = new int[input.length];
-			System.arraycopy(input, 0, output,0,input.length);
-			testPath(output);
-			return;
+	public static long[][] rightSplit(long[][] matrix, int x, int y) {
+		long[][] newmatrix = new long[matrix.length][matrix.length];
+		for(int c = 0; c < matrix.length; c++) {
+			System.arraycopy(matrix[c], 0, newmatrix[c], 0, matrix[c].length);
 		}
-		int x = input[offset];
-		for(int i = offset; i<input.length; i++) {
-			int y = input[i];
-			input[i] = x;
-			input[offset] = y;
-
-			bind(input, offset+1);
-			input[i] = y;
-		}
-		input[offset] = x;
+		newmatrix[x][y] = Long.MAX_VALUE;
+		newmatrix[y][x] = Long.MAX_VALUE;
+		OptimalTSP.reduce(newmatrix);
+		return newmatrix;
 	}
 
+	public static long[][] leftSplit(long[][] matrix, int x, int y) {
+		long[][] newmatrix = new long[matrix.length-1][matrix.length-1];
+		for(int c = 0; c < matrix.length; c++) {
+			if(c == x) {
+				c++;
+				continue;
+			}
+			System.arraycopy(matrix[c], 0, newmatrix[c], 0, y);
+			if(matrix.length != y) {
+				System.arraycopy(matrix[c], y + 1, newmatrix[c], y, matrix[c].length - y - 1);
+			}
+		}
+		newmatrix[y][x] = Long.MAX_VALUE;
+		OptimalTSP.reduce(newmatrix);
+		return newmatrix;
+	}
 }
